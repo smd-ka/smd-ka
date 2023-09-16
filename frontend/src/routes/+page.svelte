04b64c9c808f348207064c6ffd628f202991ffb0
Claus Hammann <mail@chammann.dev>
2023-09-16 18:50:21 +0200

    add neu hier

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,24 +1,64 @@
 <script lang="ts">
-	import header from '$lib/assets/header.jpg';
+	import header from '$lib/assets/header.png';
+	import groupPicture from '$lib/assets/group-picture.png';
+	import frame from '$lib/assets/frame.svg';
 	import Calendar from '$lib/components/Calendar.svelte';
 	import Insta from '$lib/components/Calendar.svelte';
 </script>
 
-<div
-	class=" h-96 bg-cover bg-[center_top_60%] brightness-105"
-	style="background-image: url({header});"
-/>
+<div class="relative">
+	<span
+		class="bg-primary absolute right-0 top-64 z-10 bg-opacity-90 px-4 py-4 text-center text-2xl text-white md:top-48 lg:py-8 lg:text-4xl"
+		>Denken. Glauben. Erleben.</span
+	>
+	<div
+		class=" h-96 bg-cover bg-[center_top_60%] brightness-105"
+		style="background-image: url({header});"
+	/>
+</div>
 
 <main class="container mx-auto flex flex-col gap-8">
 	<div class="card mt-10">
-		<h1 class="text-primary text-xl">SCHÖN, DASS DU HIER BIST!</h1>
-		<p>
-			Willkommen auf der Website der Studenten Mission Karlsruhe. Uns verbindet der Wunsch nach
-			Gemeinschaft, Diskussion und gelebtem Glauben.
-		</p>
+		<h1 class="text-primary text-2xl lg:text-4xl">Schön, dass du hier bist!</h1>
+		<div class="grid gap-4 md:grid-cols-[2fr_1fr]">
+			<div class="flex lg:text-2xl">
+				<div>
+					<p class="py-4">
+						Du startest demnächst mit deinem Studium in Karlsruhe und bist auf der Suche nach einer
+						christlichen Hochschulgruppe? <bold class="font-bold">Genial! </bold>
+					</p>
+					<p class="">
+						<a class="text-primary" href="newcomers">Hier</a> findest du alles Wichtige über die Hochschul-SMD
+						Karlsruhe und spezielle Angebote zum Semesterstart.
+					</p>
+				</div>
+			</div>
+
+			<div class="relative max-md:my-16 max-sm:my-8">
+				<img
+					class="absolute -top-4 left-8 z-10 rotate-3 scale-125 lg:left-10"
+					alt="group"
+					src={frame}
+				/>
+				<div class="px-8">
+					<img class="" alt="group" src={groupPicture} />
+				</div>
+			</div>
+		</div>
 	</div>
 
 	<div class="card">
+		<h1 class="text-primary text-4xl">Was geht? - Nächste Events</h1>
 		<Calendar />
+	</div>
+
+	<div id="about-us" class="card">
+		<h1 class="text-primary text-2xl lg:text-4xl">Über uns</h1>
+		<p class="mt-2">
+			Die Hochschul-SMD Karlsruhe ist eine überkonfessionelle, christliche Studentengruppe mit ca.
+			60 Studenten von Erstsemestern bis Doktoranden - alles ist dabei. Wir treffen uns wöchentlich
+			abwechselnd in Hauskreisen oder alle als Großgruppe bei unserem Gruppentreff. Interesse
+			geweckt? Dann schau doch mal bei einem unserer regelmäßigen Treffen vorbei!
+		</p>
 	</div>
 </main>
---
+++ frontend/src/routes/neu-hier/+page.svelte
@@ -0,0 +1,95 @@
+<script lang="ts">
+	import header from '$lib/assets/header-newcomers.png';
+</script>
+
+<div class="relative">
+	<span
+		class="bg-light-blue absolute right-0 top-64 z-10 bg-opacity-90 px-4 py-4 text-center text-2xl text-white md:top-48 lg:py-8 lg:text-4xl"
+		>Denken. Glauben. Erleben.</span
+	>
+	<h1
+		class="absolute left-1/2 top-32 z-10 -translate-x-1/2 whitespace-nowrap text-6xl text-white md:text-8xl"
+	>
+		Neu Hier?
+	</h1>
+	<div
+		class=" h-96 bg-cover bg-[center_top_34%] opacity-90 brightness-105"
+		style="background-image: url({header});"
+	/>
+</div>
+
+<main class="container mx-auto flex flex-col gap-8">
+	<div class="card mt-10 text-center">
+		<h1 class="text-primary text-2xl lg:text-4xl">Angebote für neue Studenten</h1>
+		<p class="mt-2">
+			Um unsere Gruppe kennenzulernen und gut in Karlsruhe zu starten, haben wir mehrere Angebote
+			für dich!
+		</p>
+		<div class="mt-10 grid gap-8 md:grid-cols-2">
+			<div class="rounded-lg bg-gray-200 p-4">
+				<h2 class="text-2xl font-bold">Ersti Aktionen</h2>
+				<p>
+					An den letzten Dienstagen vor dem Vorlesungsbeginn in der Regel Abends wird es spezielle
+					Aktionen für euch geben. Nähere Infos sind im <a
+						class="text-primary hover:text-corperate whitespace-nowrap hover:underline"
+						href="/kalender"
+					>
+						Kalender
+					</a> und auf Instagram zu finden. Auch zum Semesterstart wird es noch weitere Angebote geben.
+				</p>
+			</div>
+
+			<div class="rounded-lg bg-gray-200 p-4">
+				<h2 class="text-2xl font-bold">Mentoring</h2>
+				<p>
+					Du suchst jemanden zum quatschen, der dir bei Fragen rund ums Studium und das Leben in
+					Karlsruhe weiterhelfen kann? Dann melde dich bei uns und wir vermitteln dir einen Mentor.
+					Schreibe einfach eine Mail an <a
+						class="text-primary hover:text-corperate whitespace-nowrap hover:underline"
+						href="mailto:inreach@smd-karlsruhe.de"
+					>
+						inreach@smd-karlsruhe.de
+					</a>
+				</p>
+			</div>
+
+			<div class="rounded-lg bg-gray-200 p-4">
+				<h2 class="text-2xl font-bold">Church Hopping</h2>
+				<p>
+					Vielleicht bist du neu in Karlsruhe oder suchst einfach eine neue Gemeinde. Um dir bei der
+					Suche zu helfen, lädt zum Semesterstart jeden Sonntag ein SMDler dich ein, mit ihnen in
+					seine Gemeinde zu kommen, um einen ersten Einblick zu bekommen. So hast du die
+					Möglichkeit, bei dem riesigen/vielfältigen Angebot an Gemeinden in Karlsruhe dir einen
+					Überblick zu verschaffen. Checke den <a
+						class="text-primary hover:text-corperate whitespace-nowrap hover:underline"
+						href="/kalender"
+					>
+						Kalender
+					</a>
+					oder
+					<a
+						class="text-primary hover:text-corperate whitespace-nowrap hover:underline"
+						href="https://www.instagram.com/smd_karlsruhe/"
+					>
+						Instagram
+					</a> für weitere Infos.
+				</p>
+			</div>
+
+			<div class="rounded-lg bg-gray-200 p-4">
+				<h2 class="text-2xl font-bold">KA-Heim</h2>
+				<p>
+					Bist du auf der Suche nach einer christlichen WG in Karlsruhe? Auf
+					<a
+						class="text-primary hover:text-corperate whitespace-nowrap hover:underline"
+						href="https://kaheim.de"
+						>kaheim.de
+					</a>
+					betreiben wir zusammen mit dem SfC (Studierende für Christus) und anderen christlichen Gruppen
+					eine Plattform als Zimmerbörse. Wenn du ein Zimmer suchst oder in deiner WG Zimmer frei hast
+					und Mitbewohner suchst, kannst die Einträge dort durchschauen und/oder selbst einen erstellen.
+				</p>
+			</div>
+		</div>
+	</div>
+</main>
