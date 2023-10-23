389ddce944188ab2c24cf07b3635da791917d991
Claus Hammann <mail@chammann.dev>
2023-10-24 01:36:42 +0200

    footer always at the bottom

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -3,22 +3,28 @@
 	import '../app.css';
 </script>
 
-<nav class="sticky top-0 z-50 flex flex-col bg-white shadow-md">
-	<a class="flex justify-center" href="/">
-		<img class="h-14 px-4 py-2" src={logo} alt="SMD Logo" />
-	</a>
-	<div class="text-primary flex justify-center gap-2 pb-1 text-xl">
-		<a class="hover:text-corperate" href="/neu-hier">Neu Hier</a><span>-</span>
-		<a class="hover:text-corperate" href="/#about-us">Über uns</a><span>-</span>
-		<a class="hover:text-corperate" href="/kalender">Kalender</a>
-		<!-- <span>-</span> -->
-		<!-- <a class="hover:text-corperate" href="/newcomers">Intern</a> -->
+<main class="flex min-h-screen flex-col">
+	<nav class="sticky top-0 z-50 flex flex-[0_1_auto] flex-col bg-white shadow-md">
+		<a class="flex justify-center" href="/">
+			<img class="h-14 px-4 py-2" src={logo} alt="SMD Logo" />
+		</a>
+		<div class="text-primary flex justify-center gap-2 pb-1 text-xl">
+			<a class="hover:text-corperate" href="/neu-hier">Neu Hier</a><span>-</span>
+			<a class="hover:text-corperate" href="/#about-us">Über uns</a><span>-</span>
+			<a class="hover:text-corperate" href="/kalender">Kalender</a>
+			<!-- <span>-</span> -->
+			<!-- <a class="hover:text-corperate" href="/newcomers">Intern</a> -->
+		</div>
+	</nav>
+
+	<div class=" flex-[1_1_auto]">
+		<slot />
 	</div>
-</nav>
 
-<slot />
-
-<div class="bg-light-blue mt-10 flex justify-center gap-8 py-1 text-white">
-	<a href="/imprint"> Impressum </a>
-	<a class="" href="https://wiki.smd-karlsruhe.de"> Hier gehts zum Wiki </a>
-</div>
+	<footer class="flex-[0_1_auto]">
+		<div class="bg-light-blue mt-10 flex justify-center gap-8 py-1 text-white">
+			<a href="/imprint"> Impressum </a>
+			<a class="" href="https://wiki.smd-karlsruhe.de"> Hier gehts zum Wiki </a>
+		</div>
+	</footer>
+</main>
