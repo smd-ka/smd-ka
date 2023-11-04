3e3de47cb365268520812eac5e1187ada0991c54
Claus Hammann <mail@chammann.dev>
2023-11-04 23:13:11 +0100

    add theatre page

--- frontend/src/lib/components/HeroShot.svelte
+++ frontend/src/lib/components/HeroShot.svelte
@@ -6,11 +6,11 @@
 
 <div class="relative">
 	<span
-		class="{bgSlogan} absolute right-0 top-64 z-10 bg-opacity-90 px-4 py-4 text-center text-2xl text-white md:top-48 lg:py-8 lg:text-4xl"
+		class="{bgSlogan} absolute right-0 top-64 z-10 bg-opacity-90 px-4 py-4 text-center text-2xl text-white md:top-60 lg:py-8 lg:text-4xl"
 		>Denken. Glauben. Erleben.</span
 	>
 	<h1
-		class="absolute left-1/2 top-32 z-10 -translate-x-1/2 whitespace-nowrap text-6xl text-white md:text-8xl"
+		class="absolute left-1/2 top-32 z-10 -translate-x-1/2 whitespace-nowrap text-5xl text-white sm:text-6xl md:text-8xl"
 	>
 		{title || ''}
 	</h1>
---
+++ frontend/src/routes/theater/+page.svelte
@@ -0,0 +1,57 @@
+<script>
+	import HeroShot from '$lib/components/HeroShot.svelte';
+	import imgSrc from '$lib/assets/heroshots/theatre_heroshot.png';
+	import src from '$lib/assets/theatre.png';
+	import Fa from 'svelte-fa/src/fa.svelte';
+	import { faCalendar, faLocationDot } from '@fortawesome/free-solid-svg-icons';
+</script>
+
+<HeroShot {imgSrc} title="Markustheater" />
+
+<main class="card container mx-auto mt-10">
+	<h1
+		class="text-primary white text-center text-2xl tracking-[0.25em] md:tracking-[0.5em] lg:text-4xl"
+	>
+		DAS MARKUSTHEATER
+	</h1>
+
+	<div class="grid gap-4 md:grid-cols-2 md:gap-10 md:p-10 md:text-lg">
+		<div class="flex flex-col gap-4">
+			<div>
+				Das gesamte Markusevangelium in 90 Minuten – ohne Requisiten, ohne Kostüme und als
+				Rundtheater. Die Bühne? Die Mitte! Und Alles drum herum. Das bedeutet bist mittendrin und
+				erlebst die Geschichte hautnah. Sei dabei!
+			</div>
+
+			<h3 class="text-primary text-xl md:text-2xl">Aufführungen</h3>
+
+			<div class="bg-light-blue rounded-md p-4 text-white">
+				<div class="flex items-center gap-2">
+					<Fa icon={faCalendar} /> 27.11.23 um 20 Uhr
+					<span class="max-md:hidden"> (Einlass ab 19:30 Uhr)</span>
+				</div>
+				<span class="md:hidden"> (Einlass ab 19:30 Uhr)</span>
+				<div class="flex items-center gap-2">
+					<Fa icon={faLocationDot} /> Erlöserkirche Karlsruhe
+					<span class="max-md:hidden"> Hermann-Billing Straße 11</span>
+				</div>
+				<span class="md:hidden"> Hermann-Billing Straße 11</span>
+			</div>
+
+			<div class="bg-lime rounded-md p-4">
+				<div class="flex items-center gap-2">
+					<Fa icon={faCalendar} /> 27.11.23 um 20 Uhr
+					<span class="max-md:hidden"> (Einlass ab 19:30 Uhr)</span>
+				</div>
+				<span class="md:hidden"> (Einlass ab 19:30 Uhr)</span>
+				<div class="flex items-center gap-2">
+					<Fa icon={faLocationDot} /> Foyer des KIT Audimax;
+					<span class="max-md:hidden"> Straße am Forum 1</span>
+				</div>
+				<span class="md:hidden"> Straße am Forum 1</span>
+			</div>
+		</div>
+
+		<img class="rounded-md" {src} alt="Lustige Person, die Apfelschlangen isst" />
+	</div>
+</main>
