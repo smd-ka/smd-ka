4782ae8097f87dd92ff30e6b557ae4eda108be5f
Claus Hammann <mail@chammann.dev>
2024-08-03 11:15:28 +0200

    fix build

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -261,8 +261,8 @@
 					<div class="underline-a flex flex-wrap pt-1 max-md:flex-col md:gap-8">
 						<a href="/imprint">Impressum</a>
 						<a href="/privacy">Datenschutz</a>
-						<a href="/kontakt">Kontakt</a>
-						<a href="/spenden">Spenden</a>
+						<a href="/about/kontakt">Kontakt</a>
+						<a href="/about/spenden">Spenden</a>
 						<a href="https://wiki.smd-karlsruhe.de">Wiki</a>
 					</div>
 
