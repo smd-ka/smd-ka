73db30e3040fd7e2ed2cc58a2174983a29c34ba5
Claus Hammann <mail@chammann.dev>
2024-08-03 13:57:19 +0200

    finish address list

--- frontend/src/routes/intern/address-list/person/[slug]/+page.svelte
+++ frontend/src/routes/intern/address-list/person/[slug]/+page.svelte
@@ -72,55 +72,59 @@
 				alt="avatar"
 				class="border-primary mx-auto flex h-32 w-32 rounded-full object-cover"
 			/>
-			<h1 class="py-4 text-center text-2xl">{record.name} {record.surname}</h1>
-			<div class="flex items-center gap-2">
-				<Fa icon={faEnvelope} />
-				<a href={`mailto:${record.email}`}>
-					{record.email}
-				</a>
-			</div>
-			{#if record.phonenumber}
+			<h2 class="py-4 text-center uppercase">{record.name} {record.surname}</h2>
+
+			<div class="rounded-md bg-slate-100 p-4">
+				<h3>Kontakt Details</h3>
 				<div class="flex items-center gap-2">
-					<Fa icon={faPhone} />
-					<a href={`tel:${record.phonenumber}`}>
-						{record.phonenumber}
+					<Fa icon={faEnvelope} />
+					<a href={`mailto:${record.email}`}>
+						{record.email}
 					</a>
 				</div>
-			{/if}
-			{#if record.birthday}
-				<div class="flex items-center gap-2">
-					<Fa icon={faBirthdayCake} />
-					{new Date(record.birthday).toLocaleDateString()}
-				</div>
-			{/if}
-			{#if record.address}
-				<div class="flex gap-2 md:items-center">
-					<Fa class="max-md:mt-0.5" icon={faHouse} />
-					{record.address}
-				</div>
-			{/if}
-			{#if record.field_of_study}
-				<div class="flex gap-2 md:items-center">
-					<Fa class="max-md:mt-0.5" icon={faGraduationCap} />
-					{record.field_of_study}
-					{#if record.start_of_studies}
-						seit
-						{new Date(record.start_of_studies).toLocaleDateString()}
-					{/if}
-				</div>
-			{/if}
-			{#if record.team}
-				<div class="flex gap-2 md:items-center">
-					<Fa class="max-md:mt-0.5" icon={faUserGroup} />
-					SMD-Bereich: {record.team}
-				</div>
-			{/if}
-			{#if record.rili}
-				<div class="flex gap-2 md:items-center">
-					<Fa class="max-md:mt-0.5" icon={faSignature} />
-					Rili
-				</div>
-			{/if}
+				{#if record.phonenumber}
+					<div class="flex items-center gap-2">
+						<Fa icon={faPhone} />
+						<a href={`tel:${record.phonenumber}`}>
+							{record.phonenumber}
+						</a>
+					</div>
+				{/if}
+				{#if record.birthday}
+					<div class="flex items-center gap-2">
+						<Fa icon={faBirthdayCake} />
+						{new Date(record.birthday).toLocaleDateString()}
+					</div>
+				{/if}
+				{#if record.address}
+					<div class="flex gap-2 md:items-center">
+						<Fa class="max-md:mt-0.5" icon={faHouse} />
+						{record.address}
+					</div>
+				{/if}
+				{#if record.field_of_study}
+					<div class="flex gap-2 md:items-center">
+						<Fa class="max-md:mt-0.5" icon={faGraduationCap} />
+						{record.field_of_study}
+						{#if record.start_of_studies}
+							seit
+							{new Date(record.start_of_studies).toLocaleDateString()}
+						{/if}
+					</div>
+				{/if}
+				{#if record.team}
+					<div class="flex gap-2 md:items-center">
+						<Fa class="max-md:mt-0.5" icon={faUserGroup} />
+						SMD-Bereich: {record.team}
+					</div>
+				{/if}
+				{#if record.rili}
+					<div class="flex gap-2 md:items-center">
+						<Fa class="max-md:mt-0.5" icon={faSignature} />
+						Rili
+					</div>
+				{/if}
+			</div>
 		</div>
 	</div>
 {/if}
