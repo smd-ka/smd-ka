ade60056fdba838c148e2ddbc171fca5a08e202b
Claus Hammann <mail@chammann.dev>
2024-09-05 23:16:49 +0200

    init kings cafe

---
+++ kings-cafe/src/hooks.server.ts
@@ -0,0 +1,43 @@
+import { pb } from '$lib/pocketbase';
+import { redirect, type Handle } from '@sveltejs/kit';
+import { SAFT_COORDINATOR } from '$lib/pocketbase';
+
+export const handle: Handle = async ({ event, resolve }) => {
+	pb.authStore.loadFromCookie(event.request.headers.get('cookie') || '');
+
+	if (pb.authStore.isValid) {
+		try {
+			await pb.collection('users').authRefresh();
+		} catch (_) {
+			pb.authStore.clear();
+		}
+	}
+
+	event.locals.pb = pb;
+	event.locals.user = pb.authStore.model;
+
+	if (!pb.authStore.isValid && event.url.pathname.startsWith('/intern')) {
+		redirect(303, `/account/login?redirect=${event.url.pathname}`);
+	}
+
+	if (
+		!pb.authStore.model?.roles.includes(SAFT_COORDINATOR) &&
+		event.url.pathname.startsWith('/intern/saft/list')
+	) {
+		redirect(303, '/intern');
+	}
+
+	if (
+		pb.authStore.isValid &&
+		!pb.authStore.model?.verified &&
+		event.url.pathname.startsWith('/intern')
+	) {
+		redirect(303, '/account/verify');
+	}
+
+	const response = await resolve(event);
+
+	response.headers.set('set-cookie', pb.authStore.exportToCookie({ httpOnly: false }));
+
+	return response;
+};
---
+++ kings-cafe/src/routes/privacy/+page.svelte
@@ -0,0 +1,103 @@
+<main class="container mx-auto py-24 text-lg">
+	<div class="container flex flex-col gap-6 px-4 xl:px-80">
+		<h1 class="text-4xl font-bold uppercase md:hidden">Datenschutz- <br />erklärung</h1>
+		<h1 class="break-words text-5xl font-bold uppercase max-md:hidden">Datenschutzerklärung</h1>
+		<h2 class="text-3xl font-bold uppercase">Wer ist verantwortlich?</h2>
+		<p>
+			Verantwortliche Stelle für diese Seite und damit für die Erhebung deiner Daten ist die
+			Studentenmission in Deutschland (SMD) e.V.:
+		</p>
+		<ul class="border-l-primary border-l-2 px-4 py-2 text-xl">
+			<li>SMD e.V.</li>
+			<li>Universitätsstr. 30</li>
+			<li>35037 Marburg</li>
+		</ul>
+
+		<p>Unseren Datenschutzbeauftragten erreichts du unter:</p>
+		<ul class="border-l-primary border-l-2 px-4 py-2 text-xl">
+			<li>Dr. Daniel Kötz</li>
+			<li><a class="text-primary" href="mailto:datenschutz@smd.org">datenschutz@smd.org</a></li>
+		</ul>
+
+		<h2 class="text-3xl font-bold uppercase">Was für Daten erhebt ihr und warum?</h2>
+		<p>
+			Diese Seite ist explizit so gebaut, dass für den Betrieb (fast) keine Daten von dir notwendig
+			sind. Es werden lediglich die üblichen Logdateien auf dem Server geschrieben. Diese
+			beinhalten:
+		</p>
+
+		<ul class="list-disc pl-4">
+			<li class="">deine IP-Adresse</li>
+			<li class="">die Seite, die du aufrufst</li>
+			<li class="">die Uhrzeit, zu der du die Seite aufgerufen hast</li>
+			<li class="">von welcher Ursprungsseite du hierher geleitet wurdest</li>
+			<li class="">welchen Browser und welches Betriebssystem du verwendest</li>
+		</ul>
+
+		<p>
+			Diese Daten verwenden wir, um zum einen analysieren zu können, wie viele (unterschiedliche)
+			Besucher zu welcher Zeit unsere Seite besuchen (und woher sie kommen) und zum anderen, um im
+			Fall eines Angriffs nachvollziehen zu können, woher die Zugriffe kamen.
+		</p>
+
+		<p>
+			Rechtsgrundlage für die Erhebung der Daten ist einerseits das berechtigte Interesse in § 6 Nr.
+			8 DSG-EKD, um unsere Website zu verbessern und die Erfüllung einer rechtlichen Pflicht aus § 6
+			Nr. 3 DSG-EKD, da wir sicherstellen müssen, im Falle eines Angriffs adäquat reagieren und
+			diesen Angriff auswerten zu können.
+		</p>
+
+		<h2 class="text-3xl font-bold uppercase">Was passiert mit meinen Daten?</h2>
+		<p>
+			Deine Daten nutzen wir ausschließlich, um selbst etwas über die Nutzung unserer Website zu
+			lernen und z.B. festzustellen, welche unserer Artikel besonders interessant sind. <br />
+			Die Website wird in unserem Auftrag auf Servern der Mittwald CM Service netcup GmbH betrieben.
+			Im Zuge dieser Leistungserbringung ist ein Einblick in die o.g. Daten möglich. Weitere Informtionen
+			dazu erhältst du unter
+			<a href="https://www.netcup.de/kontakt/datenschutzerklaerung.php">netcup.de</a>
+		</p>
+
+		<h2 class="text-3xl font-bold uppercase">Wie lange speichert ihr meine Daten?</h2>
+		<p>
+			Deine Daten speichern wir für eine Länge von 12 Monaten. Die Speicherung erfolgt
+			ausschließlich auf Servern in Deutschland.
+		</p>
+		<h2 class="text-3xl font-bold uppercase">Welche Rechte habe ich?</h2>
+		<p>Du hast bei uns natürlich alle Rechte, die dir laut DSG-EKD zustehen! Dazu gehören u.a.:</p>
+		<ul class="list-disc pl-4">
+			<li class="">
+				Recht auf Löschung: Auf Wunsch löschen wir jederzeit alle Daten, die wir von dir erhoben
+				haben
+			</li>
+			<li class="">
+				Recht auf Auskunft: Du kannst bei uns jederzeit erfragen, welche Daten wir von dir
+				gespeichert haben
+			</li>
+			<li class="">
+				Recht auf Berichtigung: Sollten die von uns erhobenen Daten fehlerhaft sein, kannst du uns
+				stets informieren und wir berichtigen die Fehler
+			</li>
+			<li class="">
+				Recht auf Übertragung: Wenn du möchtest, können wir dir die erhobenen Daten gerne übertragen
+			</li>
+			<li class="">
+				Recht auf Einschränkung in der Verarbeitung: Wenn du möchtest, können wir deine Daten gerne
+				von der Verarbeitung ausnehmen (in dem Fall würden wir sie einfach löschen)
+			</li>
+			<li class="">
+				Recht auf Beschwerde: Du kannst dich bei einer Aufsichtsbehörde über uns beschweren – wir
+				würden uns natürlich freuen, wenn du vorher auf uns zukommst!
+			</li>
+		</ul>
+
+		<p>All diese Rechte stehen dir natürlich kostenlos zur Verfügung!</p>
+
+		<h3 class="text-2xl font-bold uppercase">An wen kann ich mich wenden?</h3>
+		<p>
+			Falls du noch Fragen hast, freut sich unser Datenschutzbeauftragter immer über Post! Du
+			erreichst ihn per E-Mail via <a class="text-primary" href="mailto:datenschutz@smd.org"
+				>datenschutz@smd.org</a
+			>
+		</p>
+	</div>
+</main>
