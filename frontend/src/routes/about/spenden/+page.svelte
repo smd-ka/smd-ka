ba1abdc3bd912bc832203ec3a801733808ad7e7d
Claus Hammann <mail@chammann.dev>
2024-09-25 18:50:02 +0200

    update donations

--- frontend/src/routes/about/spenden/+page.svelte
+++ frontend/src/routes/about/spenden/+page.svelte
@@ -1,52 +1,48 @@
 <script lang="ts">
+	import saft_group from '$lib/assets/saft/saft_group_ss24.jpg';
 </script>
 
-<main class="container mx-auto">
-	<div class="flex flex-col gap-8 px-4 py-20 xl:px-80">
-		<h1 class="text-4xl font-bold uppercase md:text-5xl">Unterstützung</h1>
-
-		<div class="grid gap-8 md:grid-cols-[1fr_3fr]">
-			<h2 class="text-3xl font-bold uppercase">Gebet</h2>
-			<p>
-				Bete, dass Gottes Liebe an unserer Hochschule sichtbar wird, Gott uns als Hochschulgruppe
-				gebraucht und Studierende Jesus kennen lernen können.
-			</p>
-
-			<h2 class="text-3xl font-bold uppercase">Mitarbeit</h2>
-			<p>
-				Du hast Interresse bei uns in der Gruppe mitzuarbeiten? Du bist herzlich willkommen!
-				Kontaktiere uns, schau bei einem unserer Gruppenabende vorbei und steig mit ein. Wir freuen
-				uns auf dich.
-			</p>
-
-			<h2 class="text-3xl font-bold uppercase">Spenden</h2>
-
-			<div class="grid gap-8">
-				<p>
-					Neben Engagement und Gebet lebt unsere Arbeit auch von finanzieller Unterstützung. Wir
-					freuen uns, wenn du uns unterstützt. Weitere Infos zur Finanzierung der SMD findest du
-					hier.
-				</p>
-
-				<ul class="font-bold">
-					<li>SMD e. V.</li>
-					<li>Evangelische Bank, Kassel</li>
-					<li>IBAN: DE75 5206 0410 0000 8004 57</li>
-					<li>Verwendungzweck: H-SMD Gruppe Karlsruhe</li>
-				</ul>
-
-				<p class="font-bold">
-					Wir danken dir für deine Spende, sie hilft uns dabei auch in Zukunft diese wichtige Arbeit
-					fortzuführen.
-				</p>
+<main class="container mx-auto grid gap-4 px-4 py-20">
+	<section class="text-center">
+		<h1 class="">Spenden</h1>
+		<p>
+			Neben Engagement und Gebet lebt unsere Arbeit auch von finanzieller Unterstützung. Wir freuen
+			uns, wenn du uns unterstützt. Deine Spende ermöglicht es uns Räume zu mieten, Vorträge zu
+			organisieren, diese Website zu betreiben und vieles mehr. Mit einer Spende unterstützt du auch
+			unsere internationale Arbeit: das King's Café.
+		</p>
+		<p class="font-bold">
+			Wir danken dir für deine Spende, sie hilft uns dabei auch in Zukunft diese wichtige Arbeit
+			fortzuführen.
+		</p>
+		<div class=" grid gap-8 py-12 md:grid-cols-2">
+			<div class="h-full w-full bg-gray-200">
+				<div class="flex h-full items-center justify-center p-4">
+					<ul class="text-left">
+						<li>SMD e. V.</li>
+						<li>Evangelische Bank, Kassel</li>
+						<li>IBAN: DE75 5206 0410 0000 8004 57</li>
+						<li>Verwendungzweck: H-SMD Gruppe Karlsruhe</li>
+					</ul>
+				</div>
+			</div>
+			<div class="relative">
+				<img src={saft_group} class="" alt="Gruppenfoto" />
+				<h3 class="absolute bottom-3 left-1/2 -translate-x-1/2 text-white">Wir sagen: Danke!</h3>
 			</div>
 		</div>
-		<div>
-			Wenn du über die aktuellen Aktionen und Anliegen der Gruppe informiert werden möchtest, kannst
-			du gerne unter dieser E-Mail-Adresse <a href="mailto:leiter@smd-karlsruhe.de"
-				>leiter@smd-karlsruhe.de</a
-			> nach unserem Freundesbrief nachfragen. Im Freundesbrief stehen alle aktuellen und wichtigen Informationen
-			sowie unsere Gebetsanliegen.
-		</div>
-	</div>
+	</section>
+
+	<section class="pad">
+		<h1 class="text-center max-sm:text-4xl">Freundesbrief</h1>
+		<p>
+			Du fühlst dich mit der SMD-Karlsruhe verbunden und bekommst noch keinen Freundesbrief? Dann
+			lass dich unbedingt auf den Verteiler setzten! Denn einmal im Semester verschicken wir an
+			alle, die möchten einen Freundesbrief. Darin berichten wir über die Arbeit der SMD-Karlsruhe
+			und geben einen Einblick in unsere Gruppe. Dort teilen wir Dankes und Gebetsanliegen sowie die
+			neusten Infos. Wenn du auch einen Freundesbrief bekommen möchtest, dann schreib gerne unseren
+			Leiter eine kurze E-Mail an
+			<a class="text-primary" href="mailto:leiter@smd-karlsruhe.de">leiter@smd-karlsruhe.de</a>.
+		</p>
+	</section>
 </main>
