4870b96493b5288499d8bfab0979e66ec1576c8e
Claus Hammann <mail@chammann.dev>
2024-09-30 12:57:29 +0200

    add description to Carousel

--- frontend/src/routes/about/angebote/+page.svelte
+++ frontend/src/routes/about/angebote/+page.svelte
@@ -1,4 +1,4 @@
-<script>
+<script lang="ts">
 	import Carousel from 'svelte-carousel';
 	// Needed b/c of this issue: https://github.com/vadimkorr/svelte-carousel/issues/141
 	// There is an open pull request https://github.com/vadimkorr/svelte-carousel/pull/142
@@ -11,49 +11,72 @@
 	import HSMD_Freizeiten from '$lib/assets/pages/about/angebote/H-SMD_Sommerfreizeiten.png';
 	import lehramtler from '$lib/assets/pages/about/angebote/lehramtler.jpg';
 	import mentoring from '$lib/assets/pages/about/angebote/mentoring.jpg';
+
 	import Fa from 'svelte-fa';
 	import {
 		faChevronLeft,
 		faChevronRight,
 		faArrowDown,
-		faArrowUpRightFromSquare
+		faArrowUpRightFromSquare,
+		faArrowUp
 	} from '@fortawesome/free-solid-svg-icons';
+	import { sineInOut } from 'svelte/easing';
+	import { fade } from 'svelte/transition';
 
 	let carouselKarlsruhe;
 	let carouselGermany;
+	let showCarouselId = 0;
+
+	const toggleShowCarousel = (id: number) => {
+		console.log(id);
+		if (showCarouselId === id) {
+			showCarouselId = 0;
+			return;
+		}
+		showCarouselId = id;
+	};
 
 	function prev(carousel) {
+		showCarouselId = 0;
 		carousel.goToPrev();
 	}
 
 	function next(carousel) {
+		showCarouselId = 0;
 		carousel.goToNext();
 	}
 
 	const slidesKarlsruhe = [
 		{
-			id: 'smd-abend',
+			id: 1,
 			title: 'SMD-Abend',
 			subtitle: 'Jeden zweiten Dienstag in großer Runde.',
-			image: SMDAbend
+			image: SMDAbend,
+			description:
+				'Alle zwei Wochen treffen wir uns dienstagabends alle zusammen, um Gott zu ehren, bei gutem Essen oder Snacks Gemeinschaft zu haben und einen Vortrag zu hören, nach dem man die Möglichkeit hat, in weiteren Gesprächen tiefer zu gehen oder einfach den Abend gemütlich ausklingen zu lassen. Die Vorträge zu verschiedenen Themen werden gehalten von ausgewählten (oft externen) Referenten. Für zeitliche und örtliche Details sieh dir gerne den Kalender an.'
 		},
 		{
-			id: 'hoersaalvortrag',
+			id: 2,
 			title: 'Hörsaalvortrag',
 			subtitle: 'Glaube und Wissenschaft. Ein Widerspruch?',
-			image: Hoersaalvortrag
+			image: Hoersaalvortrag,
+			description:
+				'Ungefähr ein- bis zweimal im Jahr veranstalten wir, oft zusammen mit dem SfC (Studierende für Christus), einen Hörsaalvortrag, der - wie soll es auch anders sein - in einem Hörsaal direkt am Campus stattfindet. Für die spannenden Vortragsthemen laden wir kompetente Referenten ein, die für jedwede Fragen Rede und Antwort stehen.'
 		},
 		{
-			id: 'gebetsfruehstueck',
+			id: 3,
 			title: 'Gebetsfrühstück',
 			subtitle: 'Gemeinsam mit Gott in den Tag starten.',
-			image: Gebetsfruehstueck
+			image: Gebetsfruehstueck,
+			description: 'Dienstags und Donnerstags gibt es ein Gebetsfrühstück.	'
 		},
 		{
-			id: 'saft',
+			id: 4,
 			title: 'SAFT',
 			subtitle: 'Semesteranfangsfreizeit: Gemeinsam ins neue Semester starten.',
-			image: SAFT
+			image: SAFT,
+			description:
+				'Die SAFT (SemesterAnfangsFreizeiT - ja, SMD ist kreativ; wir könnten auch ZKM heißen [Ziemlich Kreative Menschen; dies ist ein Karlsruher Wortwitz, Anm. d. Verf.]) findet immer zu Beginn eines jeden Semesters statt und ist eine super Gelegenheit, mal für mehr als nur 2-3 Stunden in einem intimeren Rahmen die Gruppe kennenzulernen. Für ein Wochenende fahren wir an einen Ort, an dem wir nicht andauernd an die nächste Klausur denken müssen, sondern die Gemeinschaft und fantastisches Essen genießen. Auch hier wird uns ein Referent thematisch durch das Wochenende begleiten.'
 		}
 	];
 
@@ -78,34 +101,67 @@
 
 <!-- autoplay autoplayDuration={3500} -->
 {#if browser}
-	<Carousel class="relative " bind:this={carouselKarlsruhe}>
+	<Carousel bind:this={carouselKarlsruhe}>
 		<div
 			slot="prev"
-			class="absolute z-10 ml-4 flex h-full items-center text-3xl text-white lg:text-5xl"
+			class="absolute z-10 ml-4 flex h-[50svh] items-center text-3xl text-white lg:text-5xl xl:h-[76svh]"
 		>
 			<button on:click={() => prev(carouselKarlsruhe)}> <Fa icon={faChevronLeft} /></button>
 		</div>
-		{#each slidesKarlsruhe as slide}
-			<div class="relative h-[50svh] xl:h-[76svh]">
-				<img
-					src={slide.image}
-					class="h-full w-full object-cover object-center brightness-90"
-					alt="Angebote"
-				/>
-				<div class="pad absolute top-1/3 grid w-full -translate-y-1/3 justify-center">
-					<div class="w-fit text-center text-white backdrop-filter">
-						<h1 class="pb-0">{slide.title}</h1>
-						<p class="font-caveat text-xl md:text-3xl">{slide.subtitle}</p>
-						<a class="flex items-center justify-center gap-2" href="# + {slide.id}">
-							Mehr erfahren <Fa icon={faArrowDown} />
-						</a>
+		{#each slidesKarlsruhe as carouselSlide}
+			<div>
+				<div class="relative h-[50svh] xl:h-[76svh]">
+					<img
+						src={carouselSlide.image}
+						class="h-full w-full object-cover object-center brightness-90"
+						alt="Angebote"
+					/>
+					<div class="pad absolute top-1/3 grid w-full justify-center">
+						<div class=" grid justify-items-center text-center text-white backdrop-filter">
+							<h1 class="pb-0">{carouselSlide.title}</h1>
+							<p class="font-caveat text-xl md:text-3xl">{carouselSlide.subtitle}</p>
+							<button
+								on:click={() => toggleShowCarousel(carouselSlide.id)}
+								class="flex items-center gap-2 self-center"
+							>
+								{#if showCarouselId === carouselSlide.id}
+									<div>Beschreibung ausblenden</div>
+									<Fa icon={faArrowUp} />
+								{:else}
+									<div>Mehr erfahren</div>
+									<Fa icon={faArrowDown} />
+								{/if}
+							</button>
+						</div>
+						<div>
+							{#if showCarouselId === carouselSlide.id}
+								<div
+									id={'slide' + carouselSlide.id}
+									transition:fade={{ duration: 200, easing: sineInOut }}
+									class="rounded-md bg-black bg-opacity-60 p-4 text-white backdrop-filter max-md:hidden xl:mx-60"
+								>
+									<p>{carouselSlide.description}</p>
+								</div>
+							{/if}
+						</div>
 					</div>
 				</div>
+				{#if showCarouselId === carouselSlide.id}
+					<div
+						id={'slide' + carouselSlide.id}
+						transition:fade={{ duration: 200, easing: sineInOut }}
+						class="pad bg-grey p-4 text-white md:hidden"
+					>
+						<p>
+							{carouselSlide.description}
+						</p>
+					</div>
+				{/if}
 			</div>
 		{/each}
 		<div
 			slot="next"
-			class="absolute right-0 z-10 mr-4 flex h-full items-center text-3xl text-white lg:text-5xl"
+			class="absolute right-0 z-10 mr-4 flex h-[50svh] items-center text-3xl text-white lg:text-5xl xl:h-[76svh]"
 		>
 			<button on:click={() => next(carouselKarlsruhe)}> <Fa icon={faChevronRight} /></button>
 		</div>
