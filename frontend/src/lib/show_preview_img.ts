794d25fd5c377ebf1032d543df4765c1e211e8df
Claus Hammann <mail@chammann.dev>
2024-12-08 18:27:51 +0100

    feat: remove unused image preview utility and update event editing links

--- frontend/src/lib/show_preview_img.ts
+++
@@ -1,8 +0,0 @@
-export const showPreview = (event: Event) => {
-	const files = (event.target as HTMLInputElement).files;
-	if (files && files.length > 0) {
-		src = URL.createObjectURL(files[0]);
-		image = files[0];
-	}
-	return { src, image };
-};
