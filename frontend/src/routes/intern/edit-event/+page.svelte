dce61cd99c68b36710a3fa80f82b1841917ba58c
Claus Hammann <mail@chammann.dev>
2024-12-08 16:34:47 +0100

    feat: add event editing page with event list and data loading

---
+++ frontend/src/routes/intern/edit-event/+page.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import type { PageData } from '../$types';
+
+	export let data: PageData;
+
+	let shownEvent = data.events[0];
+</script>
+
+<main class="container mx-auto px-4 py-12">
+	<div class="grid grid-cols-[32rem_1fr] gap-2 rounded-lg border p-4">
+		<section class="grid divide-y">
+			{#each data.events as event}
+				<div class="overflow-hidden text-ellipsis whitespace-nowrap">
+					<b class={shownEvent.id === event.id ? 'underline' : ''}>{event.title}</b>
+				</div>
+				<div></div>
+			{/each}
+		</section>
+		<div class="w-full bg-slate-200">Other section</div>
+	</div>
+</main>
---
+++ frontend/src/routes/intern/edit-event/+page.ts
@@ -0,0 +1,17 @@
+import { pb } from '$lib/pocketbase';
+import type { PageLoad } from './$types';
+
+export const load: PageLoad = async () => {
+	try {
+		const now = new Date();
+		const startOfToday = new Date(now.getFullYear(), now.getMonth(), now.getDate()).toISOString();
+		const records = await pb.collection('calendar').getFullList({
+			sort: '+start_date_time',
+			filter: `start_date_time >= "${startOfToday}" && category!='kingscafe'`
+		});
+		return { events: records };
+	} catch (error) {
+		console.error(error);
+		return { events: [] };
+	}
+};
