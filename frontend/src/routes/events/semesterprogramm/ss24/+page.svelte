2824405d1c46406d4b2d5bff404202793350c819
Claus Hammann <mail@chammann.dev>
2025-02-28 15:36:05 +0100

    feat: implement event loading from PocketBase and update event display logic

--- frontend/src/routes/events/semesterprogramm/ss24/+page.svelte
+++ frontend/src/routes/events/semesterprogramm/ss24/+page.svelte
@@ -1,60 +1,11 @@
 <script lang="ts">
 	import header from '$lib/assets/pages/events/semesterprogramm/herzenssache_21_9.jpg';
 	import HeroShot from '$lib/components/HeroShot.svelte';
+	import type { PageData } from '../$types';
 	import SemesterProgramSelector from '../SemesterProgramSelector.svelte';
 	import dayjs from 'dayjs';
 
-	// TODO: move this to backend and fetch it
-	let program = [
-		{
-			date: new Date('2024/4/16'),
-			title: '🫀📲 ❤️ - Wie Social Media uns beeinflusst',
-			lecturer: 'Rufina Kaehler, Regionalreferentin SMD',
-			location: 'Reinhold-Frank-Str. 44A'
-		},
-		{
-			date: new Date('2024/4/30'),
-			title: 'HERZensthema Christenverfolgung',
-			lecturer: 'Samuel Kaiser, OpenDoors',
-			location: ' Veranstaltungsort: EFG Karlsruhe, Ohiostraße 17 '
-		},
-		{
-			date: new Date('2024/5/14'),
-			title: 'Geländespiel mit HERZhaftem Picknick',
-			location: 'findet im Schlosspark statt'
-		},
-		{
-			date: new Date('2024/5/28'),
-			title: 'Mein HERZ? - Sein Geschenk',
-			lecturer: 'Rebekka, Lehrerin im Tschad',
-			location: 'Reinhold-Frank-Str. 44A'
-		},
-		{
-			date: new Date('2024/6/11'),
-			title:
-				'Zwischen GletscHERZerstörung und Konfliktherden - Hoffnung finden auf einer gefährdeten Welt',
-			lecturer: 'Prof. Dr. Peter Imming, Chemiker',
-			location: 'KIT Campus Süd'
-		},
-		{
-			date: new Date('2024/6/25'),
-			title: 'Follow your HEART - Mach einfach was dich glücklich macht? ',
-			lecturer: 'Andreas Rennig , Pfarrer',
-			location: 'Reinhold-Frank-Str. 44A'
-		},
-		{
-			date: new Date('2024/7/9'),
-			title: 'Große Fragen persönlich erlebt - ein Philosoph teilt sein HERZ',
-			lecturer: 'Heinzpeter Hempelmann, Philosoph/Theologe',
-			location: 'Reinhold-Frank-Str. 44A'
-		},
-		{
-			date: new Date('2024/7/23'),
-			title: 'Selbstfürsorge - HERZgesund leben in einer rastlosen Welt',
-			lecturer: 'Susanne Stieler, Psychologin',
-			location: 'Reinhold-Frank-Str. 44A'
-		}
-	];
+	export let data: PageData;
 </script>
 
 <HeroShot imgSrc={header} height={'h-[80svh]'} />
@@ -69,10 +20,10 @@
 	</section>
 
 	<section class="grid gap-x-24 gap-y-6 py-12 md:grid-cols-2">
-		{#each program as event}
-			<div class="flex gap-2">
+		{#each data.events as event}
+			<div class="grid grid-cols-[6rem_1fr] gap-2">
 				<h3>
-					{dayjs(event.date).format('DD.M.')}
+					{dayjs(event.start_date_time).format('DD.MM.')}
 				</h3>
 				<div>
 					<h3>
---
+++ frontend/src/routes/events/semesterprogramm/ss24/+page.ts
@@ -0,0 +1,17 @@
+import { pb } from '$lib/pocketbase';
+import type { PageLoad } from './$types';
+
+export const prerender = true;
+
+export const load: PageLoad = async () => {
+	try {
+		const records = await pb.collection('calendar').getFullList({
+			sort: '+start_date_time',
+			filter: `category='smd_abend' && start_date_time >= "2024-03-01 00:00:00" && end_date_time <= "2024-09-01 00:00:00"`
+		});
+		return { events: records };
+	} catch (error) {
+		console.error(error);
+		return { events: [] };
+	}
+};
