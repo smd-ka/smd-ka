02f4e26f1ae1ac70d4a6517cf1a98a092c32f67c
Claus Hammann <mail@chammann.dev>
2025-03-31 23:22:57 +0200

    add type for UserRecord

--- frontend/src/lib/pocketbase.ts
+++ frontend/src/lib/pocketbase.ts
@@ -29,3 +29,32 @@ export function getAvatarUrl() {
 	}
 	return src;
 }
+
+export type UserRecord = {
+	address: string;
+	allergies: string;
+	avatar: string;
+	birthday: string; // ISO date string
+	collectionId: string;
+	collectionName: string;
+	created: string; // ISO date string
+	email: string;
+	emailVisibility: boolean;
+	field_of_study: string;
+	gender: 'male' | 'female' | 'other';
+	hidden_in_addresslist: boolean;
+	id: string;
+	name: string;
+	phonenumber: string;
+	post_images: 'yes' | 'no';
+	rili: boolean;
+	roles: string[]; // Array of roles
+	start_of_studies: string; // ISO date string
+	surname: string;
+	team: string;
+	updated: string; // ISO date string
+	username: string;
+	vegetarian: boolean;
+	verified: boolean;
+	expand: Record<string, unknown>; // Expandable fields
+};
