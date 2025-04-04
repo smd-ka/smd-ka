3fcaa0b46fb717cbe47e607018878e5c491cd776
Claus Hammann <mail@chammann.dev>
2025-04-04 12:40:07 +0200

    add registration verification email templates and update mailer logic

--- backend/main.go
+++ backend/main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
+	"net/mail"
 
 	"github.com/pocketbase/pocketbase"
 	"github.com/pocketbase/pocketbase/core"
 	"github.com/pocketbase/pocketbase/plugins/ghupdate"
+	"github.com/pocketbase/pocketbase/tools/mailer"
 	"github.com/pocketbase/pocketbase/tools/template"
 )
 
@@ -18,22 +21,49 @@ func main() {
 
     app.OnMailerRecordVerificationSend().BindFunc(func(e *core.MailerRecordEvent) error {
         app.Logger().Debug("Hello")
-        e.Message.Subject = "[SMD-KA] Willkommen"
         
         registry := template.NewRegistry()
-
-
+        
+        // Overwrite System Verification email as there should be no verification link within!
+        // We use the verified property to mark the users that have been verified as know by the SMD KA
+        e.Message.Subject = "[SMD-KA] Willkommen"
         html, err := registry.LoadFiles(
             "email_templates/register_client.html",
         ).Render(map[string]any{
             "name": e.Record.Get("name"),
         })
+        if err != nil {
+            return err
+        }
+        e.Message.HTML = html
 
+        //Send Verification request email to admins
+        recordAsJson, err := json.Marshal(e.Record)
+        if err != nil {
+            return err
+        }
+        html, err = registry.LoadFiles(
+            "email_templates/register_verification.html",
+        ).Render(map[string]any{
+            "record": string(recordAsJson),
+            "link": e.App.Settings().Meta.AppURL + "/_/#/auth/confirm-verification/" + e.Meta["token"].(string),
+        })
         if err != nil {
             return err
         }
 
-        e.Message.HTML = html
+        message := &mailer.Message {
+            From: mail.Address{
+                Address: e.App.Settings().Meta.SenderAddress,
+                Name:    e.App.Settings().Meta.SenderName,
+            },
+            To:      []mail.Address{{Address: e.App.Settings().Meta.SenderAddress}},
+            Subject: "[SMD-KA] Registrierung von " + e.Record.Get("name").(string),
+            HTML:    html,
+        }
+
+        e.App.NewMailClient().Send(message)
+
 
         return e.Next()
     })
