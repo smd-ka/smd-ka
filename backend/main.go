a70d50b0f28a25248d75760b33ff0619db2e2899
Claus Hammann <mail@chammann.dev>
2025-04-21 16:14:53 +0200

    first implementation of user account creation

--- backend/main.go
+++ backend/main.go
@@ -5,17 +5,31 @@ import (
 	_ "SMD-KA-Backend/migrations"
 	"SMD-KA-Backend/registration"
 	"SMD-KA-Backend/saft"
+	"bytes"
+	"encoding/json"
+	"errors"
+	"io"
 	"log"
+	"net/http"
 	"os"
 	"strings"
 
+	"github.com/joho/godotenv"
 	"github.com/pocketbase/pocketbase"
+	"github.com/pocketbase/pocketbase/core"
 	"github.com/pocketbase/pocketbase/plugins/ghupdate"
 	"github.com/pocketbase/pocketbase/plugins/migratecmd"
 )
 
 func main() {
 	app := pocketbase.New()
+
+	// Load env file
+	// Note that app will not fail if no env is provided!
+	err := godotenv.Load()
+	if err != nil {
+		app.Logger().Error(err.Error())
+	}
 
 	// GitHub selfupdate
 	ghupdate.MustRegister(app, app.RootCmd, ghupdate.Config{})
@@ -33,6 +47,70 @@ func main() {
 	registration.SetupVerification(app)
 	saft.SaftEmails(app)
 	contact.ContactForm(app)
+
+	app.OnRecordAuthWithPasswordRequest().BindFunc(func(e *core.RecordAuthWithPasswordRequestEvent) error {
+		if e.Record == nil {
+			return errors.New("record is nil")
+		}
+
+		// Load bearer token from env
+		bearerToken := os.Getenv("AUTHENTIK_BEARER_TOKEN")
+		if bearerToken == "" {
+			app.Logger().Error("API token is not set")
+			return errors.New("missing bearer token")
+		}
+
+		url := "https://auth.smd-karlsruhe.de/api/v3/core/users/"
+		method := "POST"
+
+		// Construct the data that need to be sent to Authentik
+		// We use the email address as the username in Authentik
+
+		type Payload struct {
+			Username string `json:"username"`
+			Name     string `json:"name"`
+			Email    string `json:"email"`
+			Password string `json:"password"`
+		}
+
+		data := Payload{
+			Username: e.Record.Email(),
+			Name:     e.Record.GetString("name") + " " + e.Record.GetString("surname"),
+			Email:    e.Record.Email(),
+			Password: e.Password,
+		}
+
+		jsonData, err := json.Marshal(data)
+		if err != nil {
+			return err
+		}
+
+		req, err := http.NewRequest(method, url, bytes.NewReader(jsonData))
+		if err != nil {
+			return err
+		}
+
+		req.Header.Set("Content-Type", "application/json")
+		req.Header.Set("Accept", "application/json")
+		req.Header.Set("Authorization", "Bearer "+bearerToken)
+
+		client := &http.Client{}
+		res, err := client.Do(req)
+		if err != nil {
+			app.Logger().Error("Failed to send request to Authentik:", err)
+			return err
+		}
+		defer res.Body.Close()
+
+		body, err := io.ReadAll(res.Body)
+		if err != nil {
+			app.Logger().Error("Failed to read Authentik response:", err)
+			return err
+		}
+
+		app.Logger().Debug("Authentik response:", string(body))
+		return e.Next()
+	})
 
 	if err := app.Start(); err != nil {
 		log.Fatal(err)
