1dd055ea35732d4c227e4d88bc5d0643f94bf0d3
Claus Hammann <mail@chammann.dev>
2025-07-23 14:18:14 +0200

    remove old unused page

--- frontend/src/lib/models.ts
+++ frontend/src/lib/models.ts
@@ -65,13 +65,3 @@ export type Team = {
 	description: string;
 	image: string;
 };
-
-export type Activity = {
-	id: string;
-	collectionName: string;
-	collectionId: string;
-	title: string;
-	subtitle: string;
-	description: string;
-	image: string;
-};
