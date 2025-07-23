008a0afe5fc04ac3c43ee31282964b71d4fd95f9
Claus Hammann <mail@chammann.dev>
2025-07-23 14:18:14 +0200

    remove old unused page

---
+++ backend/migrations/1753273041_deleted_activities.go
@@ -0,0 +1,131 @@
+package migrations
+
+import (
+	"encoding/json"
+
+	"github.com/pocketbase/pocketbase/core"
+	m "github.com/pocketbase/pocketbase/migrations"
+)
+
+func init() {
+	m.Register(func(app core.App) error {
+		collection, err := app.FindCollectionByNameOrId("b6tykj9bvhpk44l")
+		if err != nil {
+			return err
+		}
+
+		return app.Delete(collection)
+	}, func(app core.App) error {
+		jsonData := `{
+			"createRule": null,
+			"deleteRule": null,
+			"fields": [
+				{
+					"autogeneratePattern": "[a-z0-9]{15}",
+					"hidden": false,
+					"id": "text3208210256",
+					"max": 15,
+					"min": 15,
+					"name": "id",
+					"pattern": "^[a-z0-9]+$",
+					"presentable": false,
+					"primaryKey": true,
+					"required": true,
+					"system": true,
+					"type": "text"
+				},
+				{
+					"autogeneratePattern": "",
+					"hidden": false,
+					"id": "0deymekm",
+					"max": 0,
+					"min": 0,
+					"name": "title",
+					"pattern": "",
+					"presentable": false,
+					"primaryKey": false,
+					"required": false,
+					"system": false,
+					"type": "text"
+				},
+				{
+					"autogeneratePattern": "",
+					"hidden": false,
+					"id": "f4uv4zni",
+					"max": 0,
+					"min": 0,
+					"name": "subtitle",
+					"pattern": "",
+					"presentable": false,
+					"primaryKey": false,
+					"required": false,
+					"system": false,
+					"type": "text"
+				},
+				{
+					"autogeneratePattern": "",
+					"hidden": false,
+					"id": "cqch1jjn",
+					"max": 0,
+					"min": 0,
+					"name": "description",
+					"pattern": "",
+					"presentable": false,
+					"primaryKey": false,
+					"required": false,
+					"system": false,
+					"type": "text"
+				},
+				{
+					"hidden": false,
+					"id": "tk2ki6md",
+					"maxSelect": 1,
+					"maxSize": 5242880,
+					"mimeTypes": null,
+					"name": "image",
+					"presentable": false,
+					"protected": false,
+					"required": false,
+					"system": false,
+					"thumbs": null,
+					"type": "file"
+				},
+				{
+					"hidden": false,
+					"id": "autodate2990389176",
+					"name": "created",
+					"onCreate": true,
+					"onUpdate": false,
+					"presentable": false,
+					"system": false,
+					"type": "autodate"
+				},
+				{
+					"hidden": false,
+					"id": "autodate3332085495",
+					"name": "updated",
+					"onCreate": true,
+					"onUpdate": true,
+					"presentable": false,
+					"system": false,
+					"type": "autodate"
+				}
+			],
+			"id": "b6tykj9bvhpk44l",
+			"indexes": [],
+			"listRule": "",
+			"name": "activities",
+			"system": false,
+			"type": "base",
+			"updateRule": null,
+			"viewRule": null
+		}`
+
+		collection := &core.Collection{}
+		if err := json.Unmarshal([]byte(jsonData), &collection); err != nil {
+			return err
+		}
+
+		return app.Save(collection)
+	})
+}
