01fc8b10a6a5cd4152f5233ea44ac4acc0696392
Claus Hammann <mail@chammann.dev>
2025-07-23 16:18:19 +0200

    improve text further

--- frontend/src/routes/new/gemeinden/+page.svelte
+++ frontend/src/routes/new/gemeinden/+page.svelte
@@ -18,20 +18,19 @@
 	</h1>
 
 	<p>
-		Die SMD-Karlsruhe ist eine übergemeindliche und überkonfessionelle Hochschulgruppe. Obwohl wir
-		unser Angebot sich mit dem einer Gemeinde an der ein oder anderen Stelle überschneidet, ist es
-		uns wichtig, dass du auch eine Gemeinde vor Ort findest, in der du dich geistlich zu Hause
-		fühlst. Wir sind der Überzeugung, dass die SMD nicht die Gemeinde ersetzen kann und soll. Die
-		SMD ist viel mehr das Bindeglied zwischen Campus und Gemeinde. Gernot Spies, der
-		SMD-Generalsekretär, drückt es folgendermaßen aus: "Wenn SMD-Arbeit nicht in die Gemeinde
-		hineinführt, dann hat sie ihr Ziel verfehlt." Aus unserer Gruppe soll missionarisches,
-		lebendiges Christsein hervorgehen, das im Horizont des Reiches Gottes steht - eingebunden in den
-		Leib Christi, verbunden mit Ortsgemeinden, angewiesen auf deren Gebet und Unterstützung.
+		Die SMD-Karlsruhe ist eine übergemeindliche und überkonfessionelle Hochschulgruppe. Uns ist es
+		wichtig, dass du auch eine Gemeinde vor Ort findest, in der du dich geistlich zu Hause fühlst.
+		Wir sind der Überzeugung, dass die SMD nicht die Gemeinde ersetzt. Die SMD ist viel mehr das
+		Bindeglied zwischen Campus und Gemeinde. Gernot Spies, der SMD-Generalsekretär, drückt es
+		folgendermaßen aus: "Wenn SMD-Arbeit nicht in die Gemeinde hineinführt, dann hat sie ihr Ziel
+		verfehlt." Aus unserer Gruppe soll missionarisches, lebendiges Christsein hervorgehen, das im
+		Horizont des Reiches Gottes steht - eingebunden in den Leib Christi, verbunden mit
+		Ortsgemeinden, angewiesen auf deren Gebet und Unterstützung.
 		<br />
 
 		<a class="text-primary fa" href="https://www.smd.org/smd/ueber-uns/smd-und-gemeinde">
 			<Fa icon={faArrowRight} />
-			Mehr zum Thema SMD und Gemeinde
+			Lies mehr, was die Zusammenarbeit von SMD und Gemeinden ausmacht.
 		</a>
 		<br />
 
@@ -39,13 +38,13 @@
 		Austausch mit den verschiedenen lokalen Gemeinden. (Eine Auflistung der evangelischen Allianz
 		findest du <a class="text-primary" href="https://evalka.de/lerne-uns-kennen"> hier</a>). Wir
 		schätzen es, dass unsere SMDler aus so vielen verschiedenen Gemeinden und Hintergründen in
-		unserer Gruppe zusammenkommen. Um dir die Suche nach einer Gemeinde etwas zu erleichtern, falls
-		du noch keine hast, findest du unten eine kleine Auswahl. Sprich gerne SMDler an und frage sie,
-		wo sie in Karlsruhe zu Hause sind. Vielleicht ist das ja auch eine gute Möglichkeit, um neue
-		Leute kennenzulernen.
+		unserer Gruppe zusammenkommen. Um dir die Suche nach einer passenden Gemeinde etwas zu
+		erleichtern findest du unten eine kleine Auswahl. Sprich gerne SMDler an und frage sie, was sie
+		an ihrer Gemeinde schätzen. Vielleicht ist das ja auch eine gute Möglichkeit, um neue Leute
+		kennenzulernen.
 		<br />
-		Am Anfang von jedem Wintersemester bieten wir das sogenannte Church Hopping an, bei dem du die Möglichkeit
-		hast, verschiedene Gemeinden in Karlsruhe kennenzulernen. Mehr dazu erfährtst du auf unserer
+		Am Anfang jedes Wintersemesters bieten wir das sogenannte "Church Hopping" an. So hast du die Möglichkeit
+		in kurzer Zeit mehrere Gemeinden kennenzulernen. Mehr dazu erfährst du auf unserer
 		<a class="text-primary" href="/new/erstsemester">Erstsemester-Seite</a>.
 	</p>
 </main>
