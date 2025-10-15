eb6dc436809205ad5b211540ae2a7fb894faa6b5
Claus Hammann <mail@chammann.dev>
2025-10-15 21:35:02 +0200

    update semester topic site

--- frontend/src/routes/events/semesterprogramm/sose25/+page.svelte
+++ frontend/src/routes/events/semesterprogramm/sose25/+page.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import header from '$lib/assets/pages/events/semesterprogramm/weitergehen_21_9.jpg';
+	import header from '$lib/assets/pages/events/semesterprogramm/voller_hoffnung_21_9.jpg';
 	import HeroShot from '$lib/components/HeroShot.svelte';
 	import dayjs from 'dayjs';
 	import type { PageData } from './$types';
@@ -12,17 +12,17 @@
 
 <main class="container mx-auto px-4 py-12">
 	<section class="">
-		<h2 class="pb-0 text-center">Unser Wintersemester 2025/26</h2>
+		<h2 class="pb-0 text-center">Unser Sommersemester 2025</h2>
 		<h3 class=" text-primary font-caveat text-center">jeden zweiten Dienstag 18:30</h3>
 
 		<p class="center pad py-6 text-center">
-			Unser Semesterthema WeiterGehen lädt dich dazu ein im Glauben einen Schritt weiter zu gehen.
-			Was heißt das? Das kann bedeuten sich mal kritisch mit dem christlichen Glauben und dem, was
-			er zu bieten hat, auseinanderzusetzen, es kann aber auch bedeuten sich zu fragen, wie man ein
-			Leben im Glauben denn jetzt genau gestaltet.
+			Das aktuelle Weltgeschehen gibt viel Anlass zur Hoffnungslosigkeit. Vielleicht hast du es auch
+			persönlich schon einmal erlebt. Wir sind davon überzeugt, dass es gute Gründe gibt zu Hoffen.
+			Bei unseren Veranstaltungen in diesem Semester kannst du entdecken, was es bedeutet Hoffnung
+			zu haben, worin sie liegt und wodurch sie begründet wird.
 
 			<br />
-			Du bist ganz herzlich eingeladen - Wir freuen uns auf dich!
+			Du bist ganz herzlich eingeladen vorbeizuschauen. Wir freuen uns auf dich!
 		</p>
 	</section>
 
--- frontend/src/routes/events/semesterprogramm/sose25/+page.ts
+++ frontend/src/routes/events/semesterprogramm/sose25/+page.ts
@@ -7,7 +7,7 @@ export const load: PageLoad = async () => {
 	try {
 		const records = await pb.collection('calendar').getFullList({
 			sort: '+start_date_time',
-			filter: `category='smd_abend' && start_date_time >= "2025-10-01 00:00:00" && end_date_time < "2026-03-01 00:00:00"`
+			filter: `category='smd_abend' && start_date_time >= "2025-03-01 00:00:00" && end_date_time < "2025-09-01 00:00:00"`
 		});
 		return { events: records };
 	} catch (error) {
