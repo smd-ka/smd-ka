983580bd8cf1f422edb6e26759f760d63c69e8af
Felix Stupp <felix.stupp@banananet.work>
2026-03-30 00:07:25 +0000

    NavlistGroup: add auth check for whole tab

--- frontend/src/lib/components/navigation/NavlistGroup.svelte
+++ frontend/src/lib/components/navigation/NavlistGroup.svelte
@@ -16,32 +16,34 @@
 	let fTab = inferNavTab(tab);
 </script>
 
-<div>
-	<h3 class="text-white">
-		<!-- manual check because of on:click -->
-		{#if fTab.defaultUrl ?? false}
-			<a href={fTab.defaultUrl} on:click={() => (showMenu = false)}>
-				{fTab.name}
-			</a>
-		{:else}
-			{fTab.name}
-		{/if}
-	</h3>
-	<button on:click={() => (showMenu = false)} class="flex flex-col text-left text-xl">
-		{#each fTab.routes as route}
-			{@const showMobile = route.showMobile ?? true}
-			{@const isAuthorized = userHasRole($currentUser, fTab.permission)}
-			{#if showMobile && isAuthorized}
-				<a class="fa ml-4" href={route.url}>
-					{route.name}
-					{#if route.extern}
-						<Fa class="text-lg" icon={faArrowUpRightFromSquare}></Fa>
-					{/if}
+{#if userHasRole($currentUser, fTab.permission)}
+	<div>
+		<h3 class="text-white">
+			<!-- manual check because of on:click -->
+			{#if fTab.defaultUrl ?? false}
+				<a href={fTab.defaultUrl} on:click={() => (showMenu = false)}>
+					{fTab.name}
 				</a>
+			{:else}
+				{fTab.name}
 			{/if}
-		{/each}
-	</button>
-</div>
+		</h3>
+		<button on:click={() => (showMenu = false)} class="flex flex-col text-left text-xl">
+			{#each fTab.routes as route}
+				{@const showMobile = route.showMobile ?? true}
+				{@const isAuthorized = userHasRole($currentUser, fTab.permission)}
+				{#if showMobile && isAuthorized}
+					<a class="fa ml-4" href={route.url}>
+						{route.name}
+						{#if route.extern}
+							<Fa class="text-lg" icon={faArrowUpRightFromSquare}></Fa>
+						{/if}
+					</a>
+				{/if}
+			{/each}
+		</button>
+	</div>
+{/if}
 
 <style>
 	h3 {
