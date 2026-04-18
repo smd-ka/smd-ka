bb68cb90600e907623e59adc3db84c2c8a1b91e7
Felix Stupp <felix.stupp@banananet.work>
2026-04-18 13:32:11 +0000

    frontend/saft/Message*: reformat

--- frontend/src/lib/components/saft/MessageAfterwards.svelte
+++ frontend/src/lib/components/saft/MessageAfterwards.svelte
@@ -1,8 +1,5 @@
 <!-- this may be rendered at the submission form & embedded into the SAFT event page -->
 <p class="font-bold">
-	Diese SAFT ist leider schon vorüber,
-	aber die nächste ist doch in Sicht!
-	Hier wirst du die Anmeldung finden,
-	sobald wir sie wieder freischalten,
-	üblicherweise 2 Wochen vor Semesterbeginn.
+	Diese SAFT ist leider schon vorüber, aber die nächste ist doch in Sicht! Hier wirst du die
+	Anmeldung finden, sobald wir sie wieder freischalten, üblicherweise 2 Wochen vor Semesterbeginn.
 </p>
